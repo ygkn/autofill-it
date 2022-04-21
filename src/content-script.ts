da3bd4e7a5313b178520942896ca6d8a461a8b68
ygkn <2000ygkn0713@gmail.com>
2022-04-22 08:24:58 +0900

    change file path

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,4 +1,4 @@
-import type { Message } from "./message";
+import type { Message } from "./types/message";
 
 type Autocompletable = HTMLElement & { autocomplete: string };
 
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,4 +1,4 @@
-import { Message } from "./message";
+import type { Message } from "./types/message";
 
 export const autocompletes = [
   { value: "on", label: "On" },
